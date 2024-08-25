b5c7232417da41d89e4d773f39ae751a9b5af517
ParadiseTeam69 <charl.becina@gmail.com>
2024-08-25 12:31:57 +0800

    sidenav close when little screen

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -2,28 +2,33 @@ import { TokenService } from './../../services/token.service';
 import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor(
-    private router: Router,
-    private _auth: AuthService,
-    private _token: TokenService
-  ) {
-    this.selectedTab = this.last_Url = this.router.url.split('/')[3];
-  }
-
   isMinimizedSidebar = false;
   last_Url: any;
   selectedTab: any;
 
+  constructor(
+    private router: Router,
+    private _auth: AuthService,
+    private _token: TokenService,
+    private breakpointObserver: BreakpointObserver
+  ) {
+    this.selectedTab = this.last_Url = this.router.url.split('/')[3];
+  }
 
   ngOnInit(): void {
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((result) => {
+        this.isMinimizedSidebar = result.matches;
+      });
   }
 
   toggleSidebar() {
@@ -34,7 +39,6 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['']);
   }
 
-
   gotoPage(actives: string) {
     this.selectedTab = actives;
     // this.router.navigate([`admin/home/${actives}`]);
@@ -43,11 +47,9 @@ export class HomeComponent implements OnInit {
   logout() {
     this._auth.logout().subscribe({
       next: (res: { logout: boolean }) => {
-        this._token.removeToken()
-        window.location.href = '/admin/login'
-      }
-    })
+        this._token.removeToken();
+        window.location.href = '/admin/login';
+      },
+    });
   }
-
-
 }
