924600e43607896d7e40a5cc52d75b6e123ed91d
SenjuHshrm <rdimasapit.bos@gmail.com>
2024-09-18 17:25:05 +0800

    temporary env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: "http://localhost:3000",
+  api: "http://147.79.67.52",
   google_client_id: '526624305554-jvbtrnjld0ihoagbkiahbo3865ng337m.apps.googleusercontent.com',
   maxImgSize: 5,
   paymongoPK: 'pk_test_xvSq6YRXYNnz81DmzqTEnb38',
